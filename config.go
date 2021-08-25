2c096d9c529038a27f3bd0973d8e7afe8d0699ff
CaoJiayuan <cjy632258@hotmail.com>
2021-08-25 16:36:20 +0800

    update

--- config.go
+++ config.go
@@ -11,71 +11,7 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-type Config interface {
-	GetValue(key string) (interface{}, bool)
-	GetInt(key string) (int, bool)
-	GetString(key string) (string, bool)
-	GetBool(key string) (bool, bool)
-}
-
-type SimpleConfig struct {
-	config map[string]interface{}
-}
-
-func (c *SimpleConfig) Load(file string) (*SimpleConfig, error) {
-	data, err := ioutil.ReadFile(file)
-
-	if e := yaml.Unmarshal(data, &c.config); e != nil {
-		return nil, e
-	}
-
-	return c, err
-}
-
-func (c *SimpleConfig) GetValue(key string) (interface{}, bool) {
-	if data, ok := c.config[key]; ok {
-		return data, true
-	}
-
-	return nil, false
-}
-
-func (c *SimpleConfig) GetInt(key string) (int, bool) {
-	if data, ok := c.GetValue(key); ok {
-		if result, o := data.(int); o {
-			return result, true
-		}
-		return 0, false
-	}
-	return 0, false
-}
-
-func (c *SimpleConfig) GetString(key string) (string, bool) {
-	if data, ok := c.GetValue(key); ok {
-		if result, o := data.(string); o {
-			return result, true
-		}
-		return "", false
-	}
-
-	return "", false
-}
-
-func (c *SimpleConfig) GetBool(key string) (bool, bool) {
-	if data, ok := c.GetValue(key); ok {
-		if result, o := data.(bool); o {
-			return result, true
-		}
-		return false, false
-	}
-	return false, false
-}
-
-func Load(file string) (*SimpleConfig, error) {
-	c := &SimpleConfig{}
-
-	return c.Load(file)
-}
+var EnvPrefix = ""
 
 func Unmarshal(file string, out interface{}) error {
 	data, err := ioutil.ReadFile(file)
@@ -121,6 +57,17 @@ func decodeEnvStruct(t reflect.Type, v reflect.Value) {
 			decodeEnvStruct(ft, fv)
 		} else if ft.Kind() == reflect.Map {
 			//
+			for _, k := range fv.MapKeys() {
+				mv := fv.MapIndex(k)
+				if mv.Kind() == reflect.Struct {
+					decodeEnvStruct(mv.Type(), mv)
+				}
+
+				if mv.Kind() == reflect.Ptr {
+					mvv := reflect.Indirect(mv)
+					decodeEnvStruct(mvv.Type(), mvv)
+				}
+			}
 		} else {
 			if key := sf.Tag.Get("env"); key != "" {
 				decodeEnv(ft, fv, key, true) // use env fisrt
@@ -134,8 +81,12 @@ func decodeEnvStruct(t reflect.Type, v reflect.Value) {
 }
 
 func decodeEnv(ft reflect.Type, fv reflect.Value, key string, prioritize bool) {
+	key = EnvPrefix + key
 	if env.GetString(key) == "" {
 		// return if env not set
+		return
+	}
+	if !fv.CanAddr() {
 		return
 	}
 
@@ -155,7 +106,7 @@ func decodeEnv(ft reflect.Type, fv reflect.Value, key string, prioritize bool) {
 }
 
 func applyDefault(ft reflect.Type, fv reflect.Value, def string) {
-	if fv.IsZero() {
+	if fv.IsZero() && fv.CanAddr() {
 		switch ft.Kind() {
 		case reflect.String:
 			fv.SetString(def)
